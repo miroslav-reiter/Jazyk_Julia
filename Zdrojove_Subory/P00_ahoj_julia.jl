d98afd2186d7405b0916c7b1deeafddc7dbba8a9
Miroslav Reiter <miroslav.reiter@uniba.sk>
2024-12-17 06:30:04 +0100

    Add files via upload

---
+++ Zdrojove_Subory/P00_ahoj_julia.jl
@@ -0,0 +1,16 @@
+# Toto je jednoriadkový komentár
+println("Ahoj Julia 👩!")
+
+# Komentar
+# pocet_studentov = 40
+pocet_studentov = 55
+
+# Dokumentacne komentare
+"""
+Funkcia `sucet(a, b)` vráti súčet dvoch čísel `a` a `b`.
+Príklad:
+    sucet(3, 5)  # Vráti 8
+"""
+function sucet(a, b)
+    return a + b
+end
